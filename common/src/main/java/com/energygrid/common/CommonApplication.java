9fb50782c8a39612a1e5c9a557e1e49c15c22d30
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2020-03-16 13:38:37 +0100

    initial setup

---
+++ common/src/main/java/com/energygrid/common/CommonApplication.java
@@ -0,0 +1,13 @@
+package com.energygrid.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
