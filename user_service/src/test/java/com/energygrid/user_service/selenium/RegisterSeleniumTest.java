4c632327cbaeecbddf5884fd03d7237bf15059e9
wezzlaren <wesley2810@hotmail.com>
2020-03-17 13:21:15 +0100

    Added selenium/webdriver dependencies + UI test

---
+++ user_service/src/test/java/com/energygrid/user_service/selenium/RegisterSeleniumTest.java
@@ -0,0 +1,42 @@
+package com.energygrid.user_service.selenium;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RegisterSeleniumTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void shouldGoToRegister() {
+        driver.navigate().to("http://localhost:4200/login");
+
+        driver.findElement(By.xpath("/html/body/app-root/app-login/mat-card/mat-card-actions[2]/button")).click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "http://localhost:4200/register");
+
+    }
+}
