abd77e1c87689049f99720c918a61981d7f2896d
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2020-03-23 11:08:25 +0100

    wip security

--- auth/src/main/java/com/energygrid/auth/AuthApplication.java
+++ auth/src/main/java/com/energygrid/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.energygrid.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
 
