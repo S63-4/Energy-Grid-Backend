5765113f4a593b359adf41cbef7afb84b3a33b97
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-03-24 09:55:55 +0100

    test

--- user_service/src/main/java/com/energygrid/user_service/controllers/UserController.java
+++ user_service/src/main/java/com/energygrid/user_service/controllers/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 public class UserController {
