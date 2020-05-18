7bab505e3ff1a6ed9b47187848925d1da4103ea8
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-05-18 12:46:36 +0200

    rabbitmq simulator

--- regional_simulator/service.py
+++ regional_simulator/service.py
@@ -5,7 +5,7 @@ import py_eureka_client.eureka_client as eureka_client
 if __name__ == "__main__":
 
     r = open("rabbitmq.txt", "r")
-    host = r
+    host = r.read()
     port = 9200
     # The flowing code will register your server to eureka server and also start to send heartbeat every 30 seconds
     f = open("eureka.txt", "r")
