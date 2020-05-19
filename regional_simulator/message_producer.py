4a1063cac3131878a74b8b6d050f3b9937497ae9
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-05-19 14:27:21 +0200

    hoi

--- regional_simulator/message_producer.py
+++ regional_simulator/message_producer.py
@@ -5,8 +5,8 @@ class MessageProducer:
     _port = None
 
     def __init__(self, host, port):
-        self._host = host
-        self._port = port
+        self._host=host
+        self._port=port
 
     def send(self, message):
         print("host:" + self._host)
