864a5fb388164310356a0c1708773f0872580f1e
lottediesveld <l.diesveld@student.fontys.nl>
2020-05-25 14:19:57 +0200

    data is stored in json file

--- data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/MessageController.java
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/MessageController.java
@@ -1,15 +1,10 @@
 package com.energygrid.datarestforwarder.RESTful;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
--- data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/consumers/MessageConsumer.java
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/consumers/MessageConsumer.java
@@ -1,12 +1,12 @@
 package com.energygrid.datarestforwarder.consumers;
 
 import com.energygrid.datarestforwarder.RESTful.MessageController;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class MessageConsumer {
     @Autowired
@@ -19,16 +19,26 @@ public class MessageConsumer {
 
     @RabbitListener(queues = "#{regionalSimQueue.name}")
     public void receiveRegional(String message) throws IOException {
-        gson.toJson(message, new FileWriter("com/energygrid/datarestforwarder/RESTful/JSONfiles/regional.json"));
+        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/regional.json");
+        writeToFile(message, jsonFile);
     }
 
     @RabbitListener(queues = "#{nationalSimQueue.name}")
     public void receiveNational(String message) throws IOException {
-        gson.toJson(message, new FileWriter("com/energygrid/datarestforwarder/RESTful/JSONfiles/national.json"));
+        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/national.json");
+        writeToFile(message, jsonFile);
     }
 
     @RabbitListener(queues = "#{marketSimQueue.name}")
     public void receiveMarket(String message) throws IOException {
-        gson.toJson(message, new FileWriter("com/energygrid/datarestforwarder/RESTful/JSONfiles/market.json"));
+        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/market.json");
+        writeToFile(message, jsonFile);
+    }
+
+    private void writeToFile(String message, File jsonFile)
+            throws IOException, JsonProcessingException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(jsonFile));
+        writer.write(message);
+        writer.close();
     }
 }
