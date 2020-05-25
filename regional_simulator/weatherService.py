8b069439caa40ba08cdc02fe1693446daa75c06d
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2020-05-25 14:14:35 +0200

    Added Weather API call

---
+++ regional_simulator/weatherService.py
@@ -0,0 +1,25 @@
+import requests, json
+
+token = open("apiToken.txt","r").read()
+
+# City code for Middelburg.
+City_code = 2750896
+
+
+def get_weather():
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?id={City_code}&appid={token}")
+
+    if response.status_code == 200:
+
+        data = json.dumps(response.json())
+        return data
+
+    else:
+        raise ConnectionAbortedError('failed to retrieve weather data.')
+
+
+
+
+
+
+
