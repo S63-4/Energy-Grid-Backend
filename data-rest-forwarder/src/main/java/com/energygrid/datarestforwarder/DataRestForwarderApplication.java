23789e1c133ad9d46b7f8ae15a4eed2f435654e6
lottediesveld <l.diesveld@student.fontys.nl>
2020-05-25 10:55:28 +0200

    created module

---
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/DataRestForwarderApplication.java
@@ -0,0 +1,13 @@
+package com.energygrid.datarestforwarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestForwarderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataRestForwarderApplication.class, args);
+    }
+
+}
