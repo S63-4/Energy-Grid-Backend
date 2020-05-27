b6a8b834b88da675ad88e5d2e1ddbb473676c227
lottediesveld <l.diesveld@student.fontys.nl>
2020-05-27 11:05:14 +0200

    changed gateway to let rest-fowarding work

--- data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/DataRestForwarderApplication.java
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/DataRestForwarderApplication.java
@@ -2,8 +2,12 @@ package com.energygrid.datarestforwarder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@EnableEurekaClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, RepositoryRestMvcAutoConfiguration.class})
 public class DataRestForwarderApplication {
 
     public static void main(String[] args) {
