6ec1c4986a17636a1477436a2aff803177808869
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-06-03 14:39:53 +0200

    paths

--- data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/consumers/MessageConsumer.java
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/consumers/MessageConsumer.java
@@ -19,19 +19,19 @@ public class MessageConsumer {
 
     @RabbitListener(queues = "#{regionalSimQueue.name}")
     public void receiveRegional(String message) throws IOException {
-        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/regional.json");
+        File jsonFile = new File("../RESTful/JSONfiles/regional.json");
         writeToFile(message, jsonFile);
     }
 
     @RabbitListener(queues = "#{nationalSimQueue.name}")
     public void receiveNational(String message) throws IOException {
-        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/national.json");
+        File jsonFile = new File("../RESTful/JSONfiles/national.json");
         writeToFile(message, jsonFile);
     }
 
     @RabbitListener(queues = "#{marketSimQueue.name}")
     public void receiveMarket(String message) throws IOException {
-        File jsonFile = new File("/Users/lottediesveld/Documents/Fontys/IntelliJ IDEA/Semester 6/Energy-Grid-Backend/data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/JSONfiles/market.json");
+        File jsonFile = new File("../RESTful/JSONfiles/market.json");
         writeToFile(message, jsonFile);
     }
 
