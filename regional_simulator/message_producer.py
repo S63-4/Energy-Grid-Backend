8f12ec7f23cbf6f71c05a3e52009124bbf0ae6bb
Wijnen,Joris J <joris.wijnen@student.fontys.nl>
2020-06-03 14:42:53 +0200

    add randomizer and blank methods for big_consumer consumption

--- regional_simulator/message_producer.py
+++ regional_simulator/message_producer.py
@@ -11,6 +11,9 @@ class MessageProducer:
     def send(self, message):
         print("host:" + self._host)
         connection = pika.BlockingConnection(pika.ConnectionParameters(host="10.93.15.85"))
+        # DEBUG CODE
+        # connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+
         channel = connection.channel()
         channel.exchange_declare(exchange="simulator-exchange", exchange_type="direct", durable=True)
         channel.basic_publish(exchange="simulator-exchange",
