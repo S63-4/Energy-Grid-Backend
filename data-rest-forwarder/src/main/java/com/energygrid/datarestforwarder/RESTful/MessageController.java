5c86dc5462571c8c4ba9e4552c06dec7e3dc972a
lottediesveld <l.diesveld@student.fontys.nl>
2020-06-03 13:20:14 +0200

    data-rest-forwarder works

--- data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/MessageController.java
+++ data-rest-forwarder/src/main/java/com/energygrid/datarestforwarder/RESTful/MessageController.java
@@ -1,6 +1,7 @@
 package com.energygrid.datarestforwarder.RESTful;
 
 import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
